8772828ac8cdb269d067d8b30d5105a599e3c318
nestorbelmar1 <nestor.belmar@gmail.com>
2026-03-26 15:50:08 -0300

    Delete polaryshr-landing/polaryshr-landing directory

--- polaryshr-landing/polaryshr-landing/components/ui/card.js
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export function Card({ className = "", children }) {
-  return <div className={cn("border bg-white", className)}>{children}</div>;
-}
-
-export function CardContent({ className = "", children }) {
-  return <div className={cn(className)}>{children}</div>;
-}
